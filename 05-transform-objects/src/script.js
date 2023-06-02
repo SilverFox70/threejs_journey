1309f3d95a0e2c4a1c5d546e2da79688a4262968
William Brinkert <william.brinkert@akqa.com>
2023-06-02 14:27:06 -0700

    [feat] 05 transforms

--- 05-transform-objects/src/script.js
+++ 05-transform-objects/src/script.js
@@ -9,10 +9,48 @@ const scene = new THREE.Scene()
 /**
  * Objects
  */
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
+const group = new THREE.Group();
+scene.add(group);
+
+const createCube = ({ size = 1, color}) => {
+  return new THREE.Mesh(
+    new THREE.BoxGeometry(size, size, size),
+    new THREE.MeshBasicMaterial({ color: color})
+  );
+}
+
+const cubeData = [
+  {
+    color: 0xff0000,
+    position: [-2, 0, 0]
+  },
+  {
+    color: 0x00ff00,
+    position: [0, 0, 0]
+  },
+  {
+    color: 0x0000ff,
+    position: [2, 0, 0]
+  }
+]
+
+cubeData.forEach(c => {
+  const cube = createCube({ color: c.color});
+  cube.position.set(c.position[0], c.position[1], c.position[2]);
+  group.add(cube);
+});
+
+
+// Scale
+
+// Rotation
+// mesh.rotation.reorder("YXZ");      // must be applied before rotations
+// mesh.rotation.y = Math.PI * 0.25;
+// mesh.rotation.x = Math.PI * 0.25;
+
+// Axes helper
+const axesHelper = new THREE.AxesHelper();
+scene.add(axesHelper);
 
 /**
  * Sizes
@@ -28,6 +66,7 @@ const sizes = {
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
 camera.position.z = 3
 scene.add(camera)
+
 
 /**
  * Renderer
