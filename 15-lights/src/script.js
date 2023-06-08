43793c5b1b6643f9a3bb7adaf8d2b0616ca47024
William Brinkert <william.brinkert@akqa.com>
2023-06-08 11:24:12 -0700

    [feat] lesson 15 lights

--- 15-lights/src/script.js
+++ 15-lights/src/script.js
@@ -17,14 +17,53 @@ const scene = new THREE.Scene()
 /**
  * Lights
  */
-const ambientLight = new THREE.AmbientLight(0xffffff, 0.5)
-scene.add(ambientLight)
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+scene.add(ambientLight);
 
-const pointLight = new THREE.PointLight(0xffffff, 0.5)
-pointLight.position.x = 2
-pointLight.position.y = 3
-pointLight.position.z = 4
-scene.add(pointLight)
+gui.add(ambientLight, "intensity")
+  .min(0)
+  .max(1)
+  .step(0.01)
+
+const directinalLight= new THREE.DirectionalLight(0x0000ff, 0.3);
+directinalLight.position.set(1, 0.25, 0)
+scene.add(directinalLight);
+
+const hemisphereLight = new THREE.HemisphereLight(0xff0000, 0x0000ff, 0.5);
+scene.add(hemisphereLight);
+
+const pointLight = new THREE.PointLight(0xff9000, 0.5, 10, 2);
+pointLight.position.set(1, - 0.5, 1);
+scene.add(pointLight);
+
+// Only works with MeshStandardMaterial and MeshPhysicalMaterial
+const rectAreaLight = new THREE.RectAreaLight(0x4e00ff, 2, 1, 1);
+rectAreaLight.position.set(- 1.5, 0, 1.5);
+rectAreaLight.lookAt(new THREE.Vector3())
+scene.add(rectAreaLight);
+
+/**
+ * Spotlight Constructor Params:
+ * color, intensity, distance, angle, penumbra, decay
+ */
+const spotLight = new THREE.SpotLight(0x78ff00, 0.5, 10, Math.PI * 0.1, 0.25, 1);
+spotLight.position.set(0, 2, 3);
+
+scene.add(spotLight);
+
+
+// Helpers
+const hemisphereLightHelper = new THREE.HemisphereLightHelper(hemisphereLight, 0.2);
+scene.add(hemisphereLightHelper);
+
+const directionalLightHelper = new THREE.DirectionalLightHelper(directinalLight, 0.2);
+scene.add(directionalLightHelper);
+
+const pointLightHelper = new THREE.PointLightHelper(pointLight, 0.2);
+scene.add(pointLightHelper);
+
+const spotLightHelper = new THREE.SpotLightHelper(spotLight);
+scene.add(spotLightHelper)
 
 /**
  * Objects
@@ -57,7 +96,7 @@ const plane = new THREE.Mesh(
 )
 plane.rotation.x = - Math.PI * 0.5
 plane.position.y = - 0.65
-
+plane.receiveShadow = true
 scene.add(sphere, cube, torus, plane)
 
 /**
